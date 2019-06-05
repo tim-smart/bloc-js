bd0b5b03d14ce77939097c5d678c5a434dd52871
Tim Smart <tim.smart@arisechurch.com>
2019-06-06 09:50:37 +1200

    Remove unused bloc context helper

--- packages/react-bloc/src/bloc-context.ts
+++
@@ -1,6 +0,0 @@
-import { useContext, Context } from "react";
-
-// Simple helper to make sure the context value is wrapped in Required
-export function useBlocContext<C extends Context<T>, T>(context: C) {
-  return useContext(context) as Required<T>;
-}
