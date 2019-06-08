8adf8d63ab440ea1a6b370840b2ef8a7b5909c3c
Tim Smart <tim.smart@arisechurch.com>
2019-06-08 14:37:31 +1200

    Fix initial state for useBlocState

--- packages/react-bloc/src/use-bloc-state.ts
+++ packages/react-bloc/src/use-bloc-state.ts
@@ -1,19 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Bloc } from "@bloc-js/bloc";
 
-export type CreateBlocFn<S> = () => Bloc<any, S>;
+export type CreateBlocFn<B> = () => B;
 
-export function useBlocState<S>(bloc: Bloc<any, S> | CreateBlocFn<S>) {
-  const [state, setState] = useState<S>();
+export function useBlocState<S, B extends Bloc<any, S>>(
+  blocCreator: B | CreateBlocFn<B>
+) {
+  const blocRef = useRef<B>();
+  function getBloc() {
+    if (blocRef.current) return blocRef.current;
+    blocRef.current =
+      typeof blocCreator === "function" ? blocCreator() : blocCreator;
+    return blocRef.current;
+  }
+
+  const [state, setState] = useState<S>(getBloc().currentState);
 
   useEffect(() => {
-    const actualBloc = typeof bloc === "function" ? bloc() : bloc;
-    setState(actualBloc.currentState);
-    const subscription = actualBloc.state$.subscribe(nextState => {
+    const shouldDispose = typeof blocCreator === "function";
+    const bloc = getBloc();
+    setState(bloc.currentState);
+    const subscription = bloc.state$.subscribe(nextState => {
       if (state === nextState) return;
       setState(nextState);
     });
-    return () => subscription.unsubscribe();
+    return () => {
+      subscription.unsubscribe();
+      if (shouldDispose) bloc.dispose();
+    };
   }, []);
 
   return state;
