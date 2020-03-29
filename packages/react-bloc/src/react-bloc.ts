0912bc0f2a86cf8e9f822b29aa8c56c77e247cec
Tim Smart <tim.smart@arisechurch.com>
2020-03-29 19:30:35 +1300

    Remove useBloc

--- packages/react-bloc/src/react-bloc.ts
+++ packages/react-bloc/src/react-bloc.ts
@@ -1,3 +1,2 @@
 export * from "./bloc-builder";
 export * from "./use-bloc-state";
-export * from "./use-bloc";
--- packages/react-bloc/src/use-bloc.ts
+++
@@ -1,6 +0,0 @@
-import { Bloc } from "@bloc-js/bloc";
-import { useMemo } from "react";
-
-export function useBloc<B extends Bloc<any>>(creator: () => B) {
-  return useMemo(creator, []);
-}
