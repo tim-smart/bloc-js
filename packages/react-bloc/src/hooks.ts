c2959fa08e07d53f2c6913b2f564abd8fb3aac5b
Tim Smart <tim.smart@arisechurch.com>
2020-12-03 10:43:14 +1300

    Add useBlocScoped, and support for extra args

--- packages/react-bloc/src/hooks.ts
+++ packages/react-bloc/src/hooks.ts
@@ -2,11 +2,29 @@ import * as React from "react";
 import { Bloc } from "@bloc-js/bloc";
 import { useEffect, useState } from "react";
 import { BlocRootContext } from "./bloc-root";
-import { BlocGetter, blocGetter, TBlocFactory } from "./registry";
+import {
+  BlocGetter,
+  blocGetter,
+  TBlocFactory,
+  BlocCompleter,
+} from "./registry";
 
-export function useBloc<S>(fn: BlocGetter<S>) {
+export function useBloc<S>(fn: BlocGetter<S>, ...args: any[]) {
   const registry = React.useContext(BlocRootContext);
-  const bloc = React.useMemo(() => fn(registry), [registry]);
+  const bloc = React.useMemo(() => fn(registry, ...args), [registry, ...args]);
+  return bloc;
+}
+
+export function useBlocScoped<S>(
+  fn: BlocGetter<S>,
+  done: BlocCompleter,
+  ...args: any[]
+) {
+  const registry = React.useContext(BlocRootContext);
+  const bloc = React.useMemo(() => fn(registry, ...args), [registry, ...args]);
+  React.useEffect(() => {
+    return () => done(registry);
+  }, [registry]);
   return bloc;
 }
 
@@ -14,20 +32,24 @@ export function useBlocState<S>(bloc: Bloc<S>) {
   const [state, setState] = useState<S>(bloc.value);
 
   useEffect(() => {
-    const subscription = bloc.subscribe(nextState => {
-      setState(nextState);
-    });
-    return () => {
-      subscription.unsubscribe();
-    };
+    const subscription = bloc.subscribe(setState);
+    return () => subscription.unsubscribe();
   }, [bloc]);
 
   return state;
 }
 
 export function createHooks<S>(id: string, factory: TBlocFactory<S>) {
-  const getBloc = blocGetter(id, factory);
-  const useBlocHook = () => useBloc(getBloc);
+  const [getBloc, done] = blocGetter(id, factory);
+  const useBlocHook = (...args: any[]) => useBloc(getBloc, ...args);
+  const useBlocScopedHook = (...args: any[]) =>
+    useBlocScoped(getBloc, done, ...args);
   const useState = () => useBlocState(useBlocHook());
-  return { getBloc, useBloc: useBlocHook, useState };
+
+  return {
+    getBloc,
+    useBloc: useBlocHook,
+    useBlocScoped: useBlocScopedHook,
+    useState,
+  };
 }
--- packages/react-bloc/src/registry.ts
+++ packages/react-bloc/src/registry.ts
@@ -18,22 +18,36 @@ export class BlocRegistry {
     );
   }
 
-  public register<S>(id: string, factory: TBlocFactory<S>) {
+  public register<S>(id: string, factory: TBlocFactory<S>, args: any[]) {
     if (this.blocs[id]) {
       return this.blocs[id] as Bloc<S>;
     }
     const state = this.initialState[id];
-    const bloc = factory(this, state);
+    const bloc = factory(this, state, ...args);
     this.blocs[id] = bloc;
     return bloc;
   }
+
+  public complete(id: string) {
+    if (!this.blocs[id]) return;
+    this.blocs[id].complete();
+    delete this.blocs[id];
+  }
 }
 
-export type TBlocFactory<S> = (r: BlocRegistry, is?: S) => Bloc<S>;
-export type BlocGetter<S> = (registry: BlocRegistry) => Bloc<S>;
+export type TBlocFactory<S> = (
+  r: BlocRegistry,
+  is?: S,
+  ...args: any[]
+) => Bloc<S>;
+export type BlocGetter<S> = (registry: BlocRegistry, ...args: any[]) => Bloc<S>;
+export type BlocCompleter = (registry: BlocRegistry) => void;
 export function blocGetter<S>(
   id: string,
   factory: TBlocFactory<S>,
-): BlocGetter<S> {
-  return registry => registry.register(id, factory);
+): [BlocGetter<S>, BlocCompleter] {
+  return [
+    (registry, ...args) => registry.register(id, factory, args),
+    registry => registry.complete(id),
+  ];
 }
